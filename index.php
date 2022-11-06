b2c4f9570023ece92dcc888ac68d421443e57388
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-06 15:42:48 +0500

    Class Constants

--- index.php
+++ index.php
@@ -1,24 +1,42 @@
-<?php
-// Class Constants
-// Constants are like variables except that once they are defined, they cannot be changed. If we need to define some constant data within a class, we need to use class constants. Class constants are case-sensitive and they can be declared inside a class with the const keyword. We can also access a constant outside of a class by using the class_name followed by :: operator with constant name. 
+<!DOCTYPE html>
+<html lang="en">
 
-// Example:
-class Namaste {
-  const GREETING_MSG= "Namaskaram Dosto!!";
-}
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
 
-echo Namaste::GREETING_MSG;
- 
+<body>
+  <?php
+  // Class Constants
+  // Constants are like variables except that once they are defined, they cannot be changed. If we need to define some constant data within a class, we need to use class constants. Class constants are case-sensitive and they can be declared inside a class with the const keyword. We can also access a constant outside of a class by using the class_name followed by :: operator with constant name. 
 
-// We can also use the self keyword to access the constant inside the class. 
-
-// Example:
-class HelloWorld {
-  const New_Message = "Did I leave the Stove on?";
-  public function ByeWorld() {
-    echo self::New_Message;
+  // Example:
+  class Namaste
+  {
+    const GREETING_MSG = "Namaskaram Dosto!!";
   }
-}
 
-$helloworld= new HelloWorld();
-$helloworld->ByeWorld();
+  echo Namaste::GREETING_MSG;
+
+
+  // We can also use the self keyword to access the constant inside the class. 
+
+  // Example:
+  class HelloWorld
+  {
+    const New_Message = "Did I leave the Stove on?";
+    public function ByeWorld()
+    {
+      echo self::New_Message;
+    }
+  }
+
+  $helloworld = new HelloWorld();
+  $helloworld->ByeWorld();
+  ?>
+</body>
+
+</html>
