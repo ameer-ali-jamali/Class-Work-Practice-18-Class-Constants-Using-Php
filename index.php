e6bf57d4a69aba4c0cbb9a29e3293f0744b300c0
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-09 19:24:57 +0500

    Constants classes practice

--- index.php
+++ index.php
@@ -1,42 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+Class Constants
+Constants are like variables except that once they are defined, they cannot be changed. If we need to define some constant data within a class, we need to use class constants. Class constants are case-sensitive and they can be declared inside a class with the const keyword. We can also access a constant outside of a class by using the class_name followed by :: operator with constant name.
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+Example:
 
-<body>
-  <?php
-  // Class Constants
-  // Constants are like variables except that once they are defined, they cannot be changed. If we need to define some constant data within a class, we need to use class constants. Class constants are case-sensitive and they can be declared inside a class with the const keyword. We can also access a constant outside of a class by using the class_name followed by :: operator with constant name. 
 
-  // Example:
-  class Namaste
+
+<?php
+class Namaste
+{
+  const GREETING_MSG = "Namaskaram Dosto!!";
+}
+
+echo Namaste::GREETING_MSG;
+?>
+
+
+We can also use the self keyword to access the constant inside the class.
+
+Example:
+
+<?php
+class HelloWorld
+{
+  const New_Message = "Did I leave the Stove on?";
+  public function ByeWorld()
   {
-    const GREETING_MSG = "Namaskaram Dosto!!";
+    echo self::New_Message;
   }
+}
 
-  echo Namaste::GREETING_MSG;
-
-
-  // We can also use the self keyword to access the constant inside the class. 
-
-  // Example:
-  class HelloWorld
-  {
-    const New_Message = "Did I leave the Stove on?";
-    public function ByeWorld()
-    {
-      echo self::New_Message;
-    }
-  }
-
-  $helloworld = new HelloWorld();
-  $helloworld->ByeWorld();
-  ?>
-</body>
-
-</html>
+$helloworld = new HelloWorld();
+$helloworld->ByeWorld();
+?>
